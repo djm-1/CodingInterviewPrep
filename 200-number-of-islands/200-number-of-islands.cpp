e22e9e4a5cc48f03dc1ff5ffebc46c0e496d75a3
Dibyajyoti Mondal <54498482+djm-1@users.noreply.github.com>
2022-05-26 13:52:28 +0530

    Time: 54 ms (35.74%), Space: 18.2 MB (14.84%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
-    
-    void dfs(vector<vector<char>>& grid, int m, int n, int i, int j){
-        
-        if(i<0 || i>=m || j<0 || j>=n)
-            return;
-        if(grid[i][j]=='0'||grid[i][j]=='2')
-            return;
-        
-        grid[i][j]='2';
-        dfs(grid,m,n,i-1,j);
-        dfs(grid,m,n,i,j-1);
-        dfs(grid,m,n,i+1,j);
-        dfs(grid,m,n,i,j+1);
-    }
     int numIslands(vector<vector<char>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        
-        int island=0;
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(grid[i][j]=='1')
-                {
-                    island++;
-                    dfs(grid,m,n,i,j);
+        int m = grid.size();
+        int n = grid[0].size();
+        int islands=0;
+        int offsets[] = {0, 1, 0, -1, 0};
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    islands++;
+                    grid[i][j] = '0';
+                    queue<pair<int, int>> todo;
+                    todo.push({i, j});
+                    while (!todo.empty()) {
+                        pair<int, int> p = todo.front();
+                        todo.pop();
+                        for (int k = 0; k < 4; k++) {
+                            int r = p.first + offsets[k], c = p.second + offsets[k + 1];
+                            if (r >= 0 && r < m && c >= 0 && c < n && grid[r][c] == '1') {
+                                grid[r][c] = '0';
+                                todo.push({r, c});
+                            }
+                        }
+                    }
                 }
-                    
             }
         }
-        
-        return island;
-    }
-};
+        return islands;
+        }
+    };
