efe93b05167e03af9ea178c6c9ae59f04a6439e4
Dibyajyoti Mondal <54498482+djm-1@users.noreply.github.com>
2022-05-31 15:48:25 +0530

    Time: 393 ms (5.17%), Space: 150 MB (27.42%) - LeetHub

---
+++ 2087-minimum-cost-homecoming-of-a-robot-in-a-grid/2087-minimum-cost-homecoming-of-a-robot-in-a-grid.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minCost(vector<int>& startPos, vector<int>& homePos, vector<int>& rowCosts, vector<int>& colCosts) {
+      
+        int cost=0;
+        // homepos cost would be included, we can't compare homepos and startpos directly. so starting from home to !start
+        for(int i=homePos[0];i!=startPos[0];i+=(i<startPos[0]?1:-1))
+            cost+=rowCosts[i];
+        for(int i=homePos[1];i!=startPos[1];i+=(i<startPos[1]?1:-1))
+            cost+=colCosts[i];
+        return cost;
+        
+    }
+};
