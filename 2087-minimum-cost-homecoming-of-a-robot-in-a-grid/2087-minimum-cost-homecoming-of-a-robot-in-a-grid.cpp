a7d1f514a8fa77c3122d64c4a7b2e633c13f7c6d
Dibyajyoti Mondal <54498482+djm-1@users.noreply.github.com>
2022-05-31 15:50:10 +0530

    Time: 413 ms (5.17%), Space: 150 MB (27.42%) - LeetHub

--- 2087-minimum-cost-homecoming-of-a-robot-in-a-grid/2087-minimum-cost-homecoming-of-a-robot-in-a-grid.cpp
+++ 2087-minimum-cost-homecoming-of-a-robot-in-a-grid/2087-minimum-cost-homecoming-of-a-robot-in-a-grid.cpp
@@ -4,6 +4,8 @@ public:
       
         int cost=0;
         // homepos cost would be included, we can't compare homepos and startpos directly. so starting from home to !start
+        
+        // seems like a dp/graph problem but actually greedy...beware pls
         for(int i=homePos[0];i!=startPos[0];i+=(i<startPos[0]?1:-1))
             cost+=rowCosts[i];
         for(int i=homePos[1];i!=startPos[1];i+=(i<startPos[1]?1:-1))
