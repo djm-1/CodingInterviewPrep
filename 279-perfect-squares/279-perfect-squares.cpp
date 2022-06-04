e1f5dfe7d3c2f415b583efb59050de8d28cef45e
Dibyajyoti Mondal <54498482+djm-1@users.noreply.github.com>
2022-06-04 20:17:53 +0530

    Time: 268 ms (42.90%), Space: 9.1 MB (44.50%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     int numSquares(int n) {
-        int root=sqrt(n);
         
-        
-        int dp[n+1];
-        memset(dp,1e9+7,sizeof(dp));
+        vector<int>dp(n+1,1e9);
         dp[0]=0;
-        dp[1]=1;
         for(int i=1;i<=n;i++)
         {
-            for(int j=0;j<=root;j++){
-                if(i>=j*j)
-                    dp[i]=min(dp[i],dp[i-j*j]+1);
+            for(int j=1;j*j<=i;j++)
+            {
+                dp[i]=min(dp[i],dp[i-j*j]+1);
             }
         }
-        // for(auto x:dp)
-        //     cout<<x<<" ";
         return dp[n];
     }
 };
