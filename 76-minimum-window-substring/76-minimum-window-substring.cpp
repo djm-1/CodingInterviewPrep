c5fab9e07beeb117d0bcc6698ea875fc8a6eab38
Dibyajyoti Mondal <54498482+djm-1@users.noreply.github.com>
2022-06-09 18:39:46 +0530

    Time: 31 ms (45.65%), Space: 7.8 MB (51.13%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -1,36 +1,38 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        unordered_map<char, int> m;
-	// Statistic for count of char in t
-	for (auto c : t) m[c]++;
-	// counter represents the number of chars of t to be found in s.
-	size_t start = 0, end = 0, counter = t.size(), minStart = 0, minLen = INT_MAX;
-	size_t size = s.size();
-	
-	// Move end to find a valid window.
-	while (end < size) {
-		// If char in s exists in t, decrease counter
-		if (m[s[end]] > 0)
-			counter--;
-		// Decrease m[s[end]]. If char does not exist in t, m[s[end]] will be negative.
-		m[s[end]]--;
-		end++;
-		// When we found a valid window, move start to find smaller window.
-		while (counter == 0) {
-			if (end - start < minLen) {
-				minStart = start;
-				minLen = end - start;
-			}
-			m[s[start]]++;
-			// When char exists in t, increase counter.
-			if (m[s[start]] > 0)
-				counter++;
-			start++;
-		}
-	}
-	if (minLen != INT_MAX)
-		return s.substr(minStart, minLen);
-	return "";
+        unordered_map<char,int>mp;
+        for(auto x:t)mp[x]++;
+        
+        int count=t.size();
+        
+        int min_len=INT_MAX;
+        int min_start=0;
+        
+        int start=0;
+        int end=0;
+        while(end<s.size())
+        {
+            if(mp[s[end]]>0)
+                count--;
+            mp[s[end]]--;
+            end++;
+            while(count==0)
+            {
+                if(end-start<min_len)
+                {
+                    min_start=start;
+                    min_len=end-start; 
+                }
+                
+                    mp[s[start]]++;
+                
+                if(mp[s[start]]>0)
+                    count++;
+                start++;
+            }
+        }
+        
+        return min_len==INT_MAX?"":s.substr(min_start,min_len);
     }
 };
