fa295057086e0aea5c327bc693c5dc038a540902
Dibyajyoti Mondal <54498482+djm-1@users.noreply.github.com>
2022-06-13 19:12:37 +0530

    Time: 79 ms (15.17%), Space: 6.6 MB (52.21%) - LeetHub

---
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minSteps(int n) {
+        
+        vector<int>dp(n+1,INT_MAX);
+        
+        dp[1]=0;
+        
+        
+        for(int i=2;i<=n;i++)
+        {
+            for(int j=1;j<i;j++)
+            {
+                if(i%j==0)
+                {
+                    dp[i]=min(dp[i],dp[j]+i/j);
+                }
+            }
+        }
+        //cout<<dp[1];
+        return dp[n];
+        
+    }
+};
