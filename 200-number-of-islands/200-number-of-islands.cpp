3517f11a60908833b310d22538e1f58a58802ff2
Dibyajyoti Mondal <54498482+djm-1@users.noreply.github.com>
2022-06-26 21:02:12 +0530

    Time: 37 ms (90.77%), Space: 12.4 MB (58.62%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,31 +1,38 @@
 class Solution {
 public:
+    
+    void dfs(vector<vector<char>>&grid, int i, int j){
+        int m=grid.size();
+        int n=grid[0].size();
+        
+        if(i<0||i>=m||j<0||j>=n||grid[i][j]=='0'||grid[i][j]=='2')
+            return;
+        grid[i][j]='2';
+        dfs(grid,i-1,j);
+        dfs(grid,i+1,j);
+        dfs(grid,i,j-1);
+        dfs(grid,i,j+1);
+        
+    }
     int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        int islands=0;
-        int offsets[] = {0, 1, 0, -1, 0};
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1') {
-                    islands++;
-                    grid[i][j] = '0';
-                    queue<pair<int, int>> todo;
-                    todo.push({i, j});
-                    while (!todo.empty()) {
-                        pair<int, int> p = todo.front();
-                        todo.pop();
-                        for (int k = 0; k < 4; k++) {
-                            int r = p.first + offsets[k], c = p.second + offsets[k + 1];
-                            if (r >= 0 && r < m && c >= 0 && c < n && grid[r][c] == '1') {
-                                grid[r][c] = '0';
-                                todo.push({r, c});
-                            }
-                        }
-                    }
+        
+        int count=0;
+        int m=grid.size();
+        int n=grid[0].size();
+        
+        
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j]=='1')
+                {
+                    dfs(grid,i,j);
+                    count++;
                 }
             }
         }
-        return islands;
-        }
-    };
+        
+        return count;
+    }
+};
