172945194440e70de94d63a92bb2cc3266051c7c
Dibyajyoti Mondal <54498482+djm-1@users.noreply.github.com>
2022-06-30 22:11:19 +0530

    Time: 648 ms (13.46%), Space: 164.2 MB (11.94%) - LeetHub

---
+++ 77-combinations/77-combinations.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    
+    void combination(vector<vector<int>>&ans,vector<int>res,int ind, int k, int n){
+        if(res.size()==k)
+        {
+            ans.push_back(res);
+            return;
+        }
+        
+        for(int i=ind;i<=n;i++)
+        {
+            res.push_back(i);
+            combination(ans,res,i+1,k,n);
+            res.pop_back();
+        }
+    }
+    
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>>ans;
+        vector<int>res;
+        
+        combination(ans,res,1,k,n);
+        
+        return ans;
+    }
+};
