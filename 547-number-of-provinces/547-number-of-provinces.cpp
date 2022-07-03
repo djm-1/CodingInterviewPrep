bc3442de1336df9bd662c0cc486c6aeb2071435a
Dibyajyoti Mondal <54498482+djm-1@users.noreply.github.com>
2022-07-03 11:15:59 +0530

    Time: 56 ms (15.07%), Space: 13.7 MB (87.67%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -2,43 +2,46 @@ class Solution {
 public:
     vector<int>parent;
     
-    int find_par(int v){
-        if(parent[v]==v)
+    int find(int v)
+    {
+        if(parent[v]==-1)
             return v;
-        else
-            return parent[v]=find_par(parent[v]);
+        return find(parent[v]);
     }
     
     
     void union_find(int u, int v){
-        parent[find_par(u)]=find_par(v);    
+        
+        int x=find(u);
+        int y=find(v);
+        
+        if(x!=y)
+            parent[y]=x;
         return;
     }
     
     int findCircleNum(vector<vector<int>>& isConnected) {
         
         int n=isConnected.size();
-        parent.resize(n);
-        
-        for(int i=0;i<n;i++)
-            parent[i]=i;
+        parent.resize(n,-1);
         
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
-            {
+            for(int j=0;j<n;j++){
                 if(isConnected[i][j]==1)
                 {
                     union_find(i,j);
                 }
             }
         }
+        
         int count=0;
-        for(int i=0;i<n;i++)
-        {
-            if(parent[i]==i)
+        for(auto x:parent){
+            cout<<x<<" ";
+            if(x==-1)
                 count++;
         }
+        
         return count;
     }
 };
