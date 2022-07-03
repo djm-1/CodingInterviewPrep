fdfa764ec45c7f4bf37dd700515be76af904f080
Dibyajyoti Mondal <54498482+djm-1@users.noreply.github.com>
2022-07-03 22:11:10 +0530

    Time: 641 ms (5.26%), Space: 14.3 MB (81.98%) - LeetHub

---
+++ 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<int>parent;
+    
+    int find(int v){
+        if(parent[v]==-1)
+            return v;
+        return parent[v]=find(parent[v]);
+    }
+    
+    
+    void union_find(int u, int v){
+        int x=find(u);
+        int y=find(v);
+        
+        if(x!=y)
+            parent[y]=x;
+    }
+    
+    int removeStones(vector<vector<int>>& stones) {
+        
+        int n=stones.size();
+        parent.resize(n,-1);
+        
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(stones[i][0]==stones[j][0]||stones[i][1]==stones[j][1])
+                {
+                    union_find(i,j);
+                }
+            }
+        }
+        
+        int comps=0;
+        for(int i=0;i<n;i++)
+        {
+            if(parent[i]==-1)
+                comps++;
+        }
+        
+        for(auto x:parent)
+            cout<<x<<" ";
+        cout<<"\n";
+        
+        return n-comps;
+    }
+};
