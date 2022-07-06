0566b4a189912f5dcfb4dbe635b57f0709eba1f1
Dibyajyoti Mondal <54498482+djm-1@users.noreply.github.com>
2022-07-06 18:56:25 +0530

    Time: 2 ms (53.03%), Space: 5.8 MB (80.09%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
+    
+    double calc(double x,int n){
+        if(n==0)
+            return 1;
+        else if(n%2==0)
+            return calc(x*x,n/2);
+        else
+            return x*calc(x*x,n/2);
+    }
+    
     double myPow(double x, int n) {
-        double res = 1;
-        while (n) {
-            if (n % 2) res = n > 0 ? res * x : res / x;
-            x = x * x;
-            n /= 2;
+        if(n<0)
+        {
+            n=abs(n);
+            x=1/x;
         }
-        return res;
+        return calc(x,n);
     }
 };
