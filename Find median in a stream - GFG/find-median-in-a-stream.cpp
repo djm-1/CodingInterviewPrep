39a59f2a13c03412985040fea25a6244986b0751
Dibyajyoti Mondal <54498482+djm-1@users.noreply.github.com>
2022-07-09 11:16:42 +0530

    Added solution - LeetHub

---
+++ Find median in a stream - GFG/find-median-in-a-stream.cpp
@@ -0,0 +1,93 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to insert heap.
+    
+    priority_queue<int>left;
+    priority_queue<int, vector<int>,greater<int>>right;
+    
+    // right will be min heap
+    // left will be max heap
+    
+    void insertHeap(int &x)
+    {
+        if(left.size()==0)
+        {
+            left.push(x);
+            return;
+        }
+            
+        if(left.size()>right.size())
+        {
+            if(x<left.top())
+            {
+                int m=left.top();
+                left.pop();
+                left.push(x);
+                right.push(m);
+            }
+            else
+            {
+                right.push(x);
+            }
+        }
+        else
+        {
+            if(x<left.top())
+            {
+                left.push(x);
+            }
+            else
+            {
+                right.push(x);
+                int m=right.top();
+                right.pop();
+                left.push(m);
+            }
+        }
+    }
+    
+    //Function to balance heaps.
+    void balanceHeaps()
+    {
+        
+    }
+    
+    //Function to return Median.
+    double getMedian()
+    {
+        if(left.size()==right.size())
+            return (double)(left.top()+right.top())/2;
+            
+        return (double)left.top();
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main()
+{
+    int n, x;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    	Solution ob;
+    	cin >> n;
+    	for(int i = 1;i<= n; ++i)
+    	{
+    		cin >> x;
+    		ob.insertHeap(x);
+    	    cout << floor(ob.getMedian()) << endl;
+    	}
+    }
+	return 0;
+}  // } Driver Code Ends
