792247abf586c4b697be2ddee039b51dc45ac6cb
Dibyajyoti Mondal <54498482+djm-1@users.noreply.github.com>
2022-08-02 00:19:17 +0530

    Time: 2579 ms (5.01%), Space: 17.3 MB (28.03%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n=nums.size();
+        
+        vector<int>dp(n,INT_MAX);
+        
+        dp[0]=0;
+        
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(j+nums[j]>=i)
+                {
+                    dp[i]=min(dp[i],dp[j]+1);
+                }
+            }
+        }
+        
+        return dp[n-1];
+    }
+};
