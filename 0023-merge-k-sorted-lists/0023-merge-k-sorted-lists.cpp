1de3b66817ca9fcecb3e2b68026ffe2ce79ad773
Dibyajyoti Mondal <54498482+djm-1@users.noreply.github.com>
2026-03-02 18:06:14 +0530

    Time: 8 ms (25.64%), Space: 19 MB (24.39%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        ListNode *dummy=new ListNode(0);
+        ListNode *ptr=dummy;
+        priority_queue<int>Q;
+        for(int i=0;i<lists.size();i++)
+        {
+            while(lists[i]!=NULL)
+            {
+                Q.push(-lists[i]->val);
+                lists[i]=lists[i]->next;
+            }
+        }
+        
+        while(!Q.empty()){
+            int curr=Q.top();
+            Q.pop();
+            ptr->next=new ListNode(-curr);
+            ptr=ptr->next;
+        }
+        
+        ListNode *head=dummy->next;
+        delete(dummy);
+        return head;
+    }
+};
