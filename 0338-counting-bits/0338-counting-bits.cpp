55d93de95cd13c9a3f6c71b8dc70fc590ae0da04
Dibyajyoti Mondal <54498482+djm-1@users.noreply.github.com>
2026-03-11 20:06:09 +0530

    Time: 2 ms (34.46%), Space: 10.5 MB (80.35%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
+    int count_setbit(int n){
+        int cnt=0;
+        while(n!=0){
+            n&=(n-1);
+            cnt++;
+        }
+        return cnt;
+    }
     vector<int> countBits(int n) {
         vector<int>ans(n+1);
         for(int i=0;i<=n;i++)
         {
-            ans[i]=__builtin_popcount(i);
+            ans[i]=count_setbit(i);
         }
 
         return ans;
